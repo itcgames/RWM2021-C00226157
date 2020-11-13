7659507e4922cb2df0c383833b69428ad692f663
PatrickDarcy <C00226157@itcarlow.ie>
2020-11-13 17:32:39 +0000

    Player Movement in progress test not complete

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private const float MAX_SPEED = 1.0f;
+    private Vector3 speed = new Vector2(0.0f, 0.0f);
+    private const float ACCELERATION = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            speed = movePlayer(KeyCode.W);
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            speed = movePlayer(KeyCode.A);
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            speed = movePlayer(KeyCode.S);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            speed = movePlayer(KeyCode.D);
+        }
+
+        transform.position += speed;
+    }
+
+    public Vector3 movePlayer(KeyCode t_input)
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (speed.y < MAX_SPEED)
+            {
+                speed.y += ACCELERATION;
+            }
+            if (speed.x < 0)
+            {
+                speed.x += ACCELERATION;
+            }
+            if (speed.x > 0)
+            {
+                speed.x -= ACCELERATION;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (speed.x > -MAX_SPEED)
+            {
+                speed.x -= ACCELERATION;
+            }
+            if (speed.y < 0)
+            {
+                speed.y += ACCELERATION;
+            }
+            if (speed.y > 0)
+            {
+                speed.y -= ACCELERATION;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (speed.y > -MAX_SPEED)
+            {
+                speed.y -= ACCELERATION;
+            }
+            if (speed.x < 0)
+            {
+                speed.x += ACCELERATION;
+            }
+            if (speed.x > 0)
+            {
+                speed.x -= ACCELERATION;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (speed.x < MAX_SPEED)
+            {
+                speed.x += ACCELERATION;
+            }
+            if (speed.y < 0)
+            {
+                speed.y += ACCELERATION;
+            }
+            if (speed.y > 0)
+            {
+                speed.y -= ACCELERATION;
+            }
+        }
+
+        transform.position += speed;
+        return speed;
+    }
+}
---
+++ Assets/Tests/PlayerMovementTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PlayerMovementTest
+    {
+        private PlayerScript player;
+
+        [SetUp]
+        public void Setup()
+        {
+            GameObject gameGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            player = gameGameObject.GetComponent<PlayerScript>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(player.gameObject);
+        }
+        // A Test behaves as an ordinary method
+        [UnityTest]
+        public IEnumerator PlayerMovementSimplePasses()
+        {
+            KeyCode[] input = { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
+            Vector3[] output = new Vector3[4];
+            for (int i = 0; i < 4; i++)
+            {
+               output[i] = player.movePlayer(input[i]);
+            }
+            Vector3[] expected = { new Vector3(0.0f, 0.1f), new Vector3(-0.1f, 0.0f), new Vector3(0.0f, -0.1f), new Vector3(0.1f, 0.0f) };
+
+            CollectionAssert.AreEqual(expected, output);
+
+            yield return null;
+        }
+    }
+}
